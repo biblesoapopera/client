3314cca00a95a6de97d21fdb8a5f8cd29228eceb
Tim Roediger <superdweebie@gmail.com>
2014-11-20 21:19:45 +1100

    Top nav menu much improved

--- src/js/runEpisode.js
+++ src/js/runEpisode.js
@@ -1,13 +1,41 @@
 bso.runEpisode = function(episodeData){
   
   var sectionIndex = 0;
-  var slideIndex = 0;
+  var slideIndex = -1;
   
   //setup top-nav  
   document.querySelector('.top-nav div.title').innerHTML = episodeData.title;
   
+  var topNav = document.querySelector('.top-nav');
+  var sectionDiv;
+  var classAttr;
+  for (var i in episodeData.sections){
+    sectionDiv = document.createElement('div');
+    
+    classAttr = 'section ' + episodeData.sections[i].type;    
+    if (i==0) classAttr += ' active';   
+    sectionDiv.setAttribute('class', classAttr);    
+    sectionDiv.innerHTML = episodeData.sections[i].title;    
+    topNav.appendChild(sectionDiv);    
+    episodeData.sections[i].topNavNode = sectionDiv;
+  }
+  
   function next(){
   
+    ++slideIndex;
+    if(slideIndex === episodeData.sections[sectionIndex].slides.length){
+        episodeData.sections[sectionIndex].topNavNode.setAttribute(
+            'class',
+            episodeData.sections[sectionIndex].topNavNode.getAttribute('class').replace(' active', '')    
+        );
+        ++sectionIndex;        
+        episodeData.sections[sectionIndex].topNavNode.setAttribute(
+            'class',
+            episodeData.sections[sectionIndex].topNavNode.getAttribute('class') + ' active'
+        );        
+        slideIndex = 0;
+    }        
+     
     if(!(slideIndex === episodeData.sections[sectionIndex].slides.length-1 && sectionIndex === episodeData.sections.length-1)){
        bso.next.hide();
     }
@@ -18,13 +46,7 @@ bso.runEpisode = function(episodeData){
     
     var slide = new bso.slide[type](config);
 
-    bso.slideTransition(slide, 'right');
-    
-    ++slideIndex;    
-    if(slideIndex === episodeData.sections[sectionIndex].slides.length){
-        ++sectionIndex;
-        slideIndex = 0;
-    }
+    bso.slideTransition(slide, 'right'); 
   }  
   
   bso.next.create(next);
