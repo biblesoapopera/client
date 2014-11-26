d6728c3d8e42e41d0be2c42e2e91310dc4edf875
Tim Roediger <superdweebie@gmail.com>
2014-11-26 22:04:02 +1100

    WIP audio slide

--- src/js/slide/audio.js
+++ src/js/slide/audio.js
@@ -1,28 +1,51 @@
 bso.slide.audio = function(config){
     this.config = config;
-    
+        
+    var playing = false;
     var player = new Audio();
-    player.src = config.audioUrl;
- 
+    player.src = config.audioUrl; 
+    
+    function toggle(){
+        if (playing){
+            player.pause();
+            playing = false;
+            playPauseBtn.setAttribute('class', 'btn play');
+        } else if (player.currentTime < config.end) {
+            player.play();
+            playing = true;
+            playPauseBtn.setAttribute('class', 'btn pause');            
+        }        
+    }
+    
     var template = document.querySelector('[data-slide=audio]');    
     var clone = document.importNode(template.content, true);
-    clone.querySelector('[data-attach=play]').addEventListener('click', function(){
-        player.play();
-    });
-    clone.querySelector('[data-attach=pause]').addEventListener('click', function(){
-        player.pause();
-    });
-    clone.querySelector('[data-attach=plus]').addEventListener('click', function(){
+    
+    var playPauseBtn = clone.querySelector('.play');
+    playPauseBtn.addEventListener('click', toggle);
+
+    clone.querySelector('.vol-plus').addEventListener('click', function(){
         if (player.volume > 0.8) player.volume = 1
         player.volume+=0.2;
     });
-    clone.querySelector('[data-attach=minus]').addEventListener('click', function(){        
+    clone.querySelector('.vol-minus').addEventListener('click', function(){        
         if (player.volume < 0.2) player.volume = 0
         else player.volume-=0.2;
     });
     
     document.body.appendChild(clone);
-    this.node = document.body.lastElementChild;        
+    this.node = document.body.lastElementChild;       
+    
+    player.addEventListener('timeupdate', function(evt){
+        if (player.currentTime >= config.end) {
+            toggle();
+            player.currentTime = config.start;
+        }
+    })
+    
+    setTimeout(function(){
+       player.currentTime = config.start;
+       toggle();
+    }, 1000);
 }
 
 //                    <button data-attach='play' ick="document.getElementById('demo').play()">Play the Audio</button>
