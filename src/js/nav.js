20e6c02fee4b7fb9d9e7cc4e71908f4caf21a1a7
Tim Roediger <superdweebie@gmail.com>
2014-11-30 20:50:09 +1100

    remove duplicate code in next.js and previous.js

---
+++ src/js/nav.js
@@ -0,0 +1,40 @@
+bso.nav = function(name, dir){
+    return {
+        create: function(goFn){
+           var btn = document.createElement('a');
+           btn.setAttribute('class', 'btn ' + name);
+           btn.addEventListener('click', function(){
+               goFn(dir);
+           });
+           document.body.appendChild(btn);
+           this.disabled = false;
+           this.hidden = false;
+        },
+        render: function(){
+           var cls = 'btn ' + name;
+           if (this.disabled) cls += ' disabled'
+           if (this.hidden) cls += ' hidden'
+           document.querySelector('.btn.' + name).setAttribute('class', cls);        
+        },
+        show: function(){
+           this.hidden = false;
+           this.render();
+        },
+        hide: function(){    
+           this.hidden = true;
+           this.render();       
+        },
+        enable: function(){        
+           this.disabled = false;
+           this.render();       
+        },
+        disable: function(){
+           this.disabled = true;
+           this.render();        
+        }
+    }
+}
+
+bso.next = bso.nav('next', 1);
+
+bso.previous = bso.nav('previous', -1);
