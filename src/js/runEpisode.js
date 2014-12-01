6799d30101eb2bda6227c861cc75c9fa6f760273
Tim Roediger <superdweebie@gmail.com>
2014-12-01 22:34:00 +1100

    WIP top-nav

--- src/js/runEpisode.js
+++ src/js/runEpisode.js
@@ -2,24 +2,10 @@ bso.runEpisode = function(episodeData){
   
   var sectionIndex = 0;
   var slideIndex = -1;
-  
-  //setup top-nav  
-  document.querySelector('.top-nav div.title').innerHTML = episodeData.title;
-  
-  var topNav = document.querySelector('.top-nav');
-  var sectionDiv;
-  var classAttr;
-  for (var i in episodeData.sections){
-    sectionDiv = document.createElement('div');
-    
-    classAttr = 'section ' + episodeData.sections[i].type;    
-    if (i==0) classAttr += ' active';   
-    sectionDiv.setAttribute('class', classAttr);    
-    sectionDiv.innerHTML = episodeData.sections[i].title;    
-    topNav.appendChild(sectionDiv);    
-    episodeData.sections[i].topNavNode = sectionDiv;
-  }
-    
+
+  //setup top-nav     
+  bso.topNav(episodeData);
+     
   var slideCache = [];
   function go(dir){
   
---
+++ src/js/topNav.js
@@ -0,0 +1,18 @@
+bso.topNav = function(episodeData){
+  
+//  document.querySelector('.top-nav div.title').innerHTML = episodeData.title;
+//  
+//  var topNav = document.querySelector('.top-nav');
+//  var sectionDiv;
+//  var classAttr;
+//  for (var i in episodeData.sections){
+//    sectionDiv = document.createElement('div');
+//    
+//    classAttr = 'section ' + episodeData.sections[i].type;    
+//    if (i==0) classAttr += ' active';   
+//    sectionDiv.setAttribute('class', classAttr);    
+//    sectionDiv.innerHTML = episodeData.sections[i].title;    
+//    topNav.appendChild(sectionDiv);    
+//    episodeData.sections[i].topNavNode = sectionDiv;
+//  }    
+}
