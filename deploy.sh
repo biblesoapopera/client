001f00ffb493e9a45845233475ca262337cba7d2
Tim Roediger <superdweebie@gmail.com>
2015-06-06 14:49:08 +1000

    correct name of .travis.yml

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,13 @@
 set -e # exit with nonzero exit code if anything fails
 
 echo "inside deploy script!"
-ls
+
+cd dist
+
+git init
+git config user.name "Travis CI"
+git config user.email "superdweebie@gmail.com"
+git add .
+git commit -m "Travis auto deploy to gh-pages"
+
+git push --force --quiet "https:://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
