bcc9cd0bff395b96a0700f0d027f6e8fce30965a
Tim Roediger <superdweebie@gmail.com>
2015-08-05 22:13:09 +1000

    audio slide working with new ui

--- src/Screen/audio/audio.js
+++ src/Screen/audio/audio.js
@@ -7,6 +7,7 @@ $.screen.audio = function(config){
   this.action = this.content.querySelector('.btn');
   this.action.addEventListener('click', this);
 
+  this.track = this.content.querySelector('.track');
   this.grip = this.content.querySelector('.grip');
   this.grip.addEventListener('mousedown', this);
   this.grip.addEventListener('touchstart', this);
@@ -21,7 +22,20 @@ $.screen.audio = function(config){
 $.extend($.screen.slide, $.screen.audio);
 
 $.screen.audio.prototype.enter = function(){
+
+  //TODO remove this line. It makes audio skippable for demo purposes only
+  this.complete = true;
+
   $.screen.slide.prototype.enter.call(this);
+
+  this.player.currentTime = this.currentTime;
+  this.player.addEventListener('timeupdate', this);
+}
+
+$.screen.audio.prototype.exit = function(){
+  this.player.removeEventListener('timeupdate', this);
+  if (this.action.classList.contains('pause')) this.pause();
+  this.currentTime = this.player.currentTime;
 }
 
 $.screen.audio.prototype.handleEvent = function(evt){
@@ -33,4 +47,98 @@ $.screen.audio.prototype.handleEvent = function(evt){
   else if (evt.type === 'touchend') this.dragend(evt)
   else if (evt.type === 'mousemove') this.dragmove(evt)
   else if (evt.type === 'touchmove') this.dragmove(evt)
+}
+
+$.screen.audio.prototype.handleClick = function(evt){
+  if (evt.target === this.action) this.actionClick(evt)
+  else if (evt.target === this.progress) this.progressClick(evt)
+}
+
+$.screen.audio.prototype.actionClick = function(){
+  if (this.action.classList.contains('play')) this.play()
+  else this.pause()
+}
+
+$.screen.audio.prototype.progressClick = function(evt){
+  var currentTime =
+    this.config.start +
+    (evt.clientX - this.progress.getBoundingClientRect().left) * (this.config.end - this.config.start) / 300;
+
+  if (currentTime < this.config.start) currentTime = this.config.start
+  else if (currentTime > this.config.end) currentTime = this.config.end
+  this.player.currentTime = currentTime;
+}
+
+$.screen.audio.prototype.play = function(){
+  this.player.play();
+  this.action.classList.remove('play');
+  this.action.classList.add('pause');
+}
+
+$.screen.audio.prototype.pause = function(){
+  this.player.pause();
+  this.action.classList.remove('pause');
+  this.action.classList.add('play');
+}
+
+$.screen.audio.prototype.timeupdate = function(){
+  if (this.player.currentTime >= this.config.end) {
+    this.pause();
+    this.player.currentTime = this.config.start;
+    this.complete = true;
+  }
+
+  var percProgress = (this.player.currentTime - this.config.start)/(this.config.end - this.config.start);
+
+  if (percProgress < 0) percProgress = 0
+  else if (percProgress > 1) percProgress = 1
+
+  this.grip.style.left = (this.track.getBoundingClientRect().width * percProgress - this.grip.getBoundingClientRect().width / 2) * 100 / document.documentElement.clientWidth + 'vw';
+
+  if (this.progressTime < this.player.currentTime){
+    this.progressTime = this.player.currentTime;
+    this.progress.style.width = this.track.getBoundingClientRect().width * percProgress * 100 / document.documentElement.clientWidth + 'vw';
+  }
+}
+
+$.screen.audio.prototype.dragstart = function(evt){
+  this.isPlaying = !this.player.paused;
+  this.player.pause();
+  this.gripDragPosition = {
+    left: parseFloat(this.grip.style.left.replace('vw', '')) * document.documentElement.clientWidth / 100,
+    client: $.getClientX(evt)
+  };
+  ['mouseup','mousemove','touchend','touchmove'].forEach(function(name){
+    document.addEventListener(name, this)
+  }.bind(this));
+}
+
+$.screen.audio.prototype.dragend = function(){
+  ['mouseup','mousemove','touchend','touchmove'].forEach(function(name){
+    document.removeEventListener(name, this)
+  }.bind(this));
+
+  var currentTime =
+    this.config.start + (this.gripDragPosition.left + this.grip.getBoundingClientRect().width / 2) * (this.config.end - this.config.start) / this.track.getBoundingClientRect().width;
+
+  if (currentTime < this.config.start) currentTime = this.config.start
+  else if (currentTime > this.config.end) currentTime = this.config.end
+
+  this.player.currentTime = currentTime;
+  if (this.isPlaying) this.player.play();
+}
+
+$.screen.audio.prototype.dragmove = function(evt){
+  var clientX = $.getClientX(evt);
+  var newLeft = this.gripDragPosition.left + clientX - this.gripDragPosition.client;
+
+  if (newLeft < -this.grip.getBoundingClientRect().width / 2){
+    newLeft = -this.grip.getBoundingClientRect().width / 2;
+  } else if (newLeft > this.progress.getBoundingClientRect().width - this.grip.getBoundingClientRect().width / 2){
+    newLeft = this.progress.getBoundingClientRect().width - this.grip.getBoundingClientRect().width / 2;
+  }
+
+  this.grip.style.left = newLeft * 100 / document.documentElement.clientWidth + 'vw';
+  this.gripDragPosition.left = newLeft;
+  this.gripDragPosition.client = clientX;
 }
